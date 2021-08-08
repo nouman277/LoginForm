8a07fb456d17860ac12530fba65bf02cbf2beeac
nouman277 <n.noumannoor@gmail.com>
2021-08-08 16:01:59 +0500

    login form with another activity_show_print.xml

--- app/src/main/java/com/example/loginform1/MainActivity.java
+++ app/src/main/java/com/example/loginform1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.loginform1;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText usernameEditText;
     private EditText passwordEditText;
     private Button loginButton;
+    private String usererr,passerr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +22,17 @@ public class MainActivity extends AppCompatActivity {
         usernameEditText = findViewById(R.id.activity_main_usernameEditText);
         passwordEditText = findViewById(R.id.activity_main_passwordEditText);
         loginButton = findViewById(R.id.activity_main_loginButton);
+        usererr ="user name cannot be empty";
+        passerr = "password must be more then six character";
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (passwordEditText.getText().length() <= 6){
-                    String toastMessage = "Password Must Be More then Six Charters";
-                    Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this,show_print.class);
+                    intent .putExtra("keypasserr",passerr);
+                    startActivity(intent);
+
                 }
                 else if (
                         usernameEditText.getText().length() > 0 && passwordEditText.getText().length() > 0) {
@@ -34,10 +40,13 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
                 }
 
-                else {
-                    String toastMessage = "Username Cannot Be Empty ";
-                    Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
+                else {  Intent intent = new Intent(MainActivity.this,show_print.class);
+                    intent .putExtra("usererr",usererr);
+                    startActivity(intent);
+
+
                 }
+
             }
         });
     }
